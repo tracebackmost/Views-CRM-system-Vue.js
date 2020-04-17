5987795cc27599dbd19edf8a76f9b2a02c4257fa
tracebackmost <63810296+tracebackmost@users.noreply.github.com>
2020-04-17 11:58:31 +0300

    Add files via upload

---
+++ Profile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Профиль</h3>
+    </div>
+
+    <form class="form" @submit.prevent="submitHandler">
+      <div class="input-field">
+        <input
+            id="description"
+            type="text"
+            v-model="name"
+            :class="{invalid: $v.name.$dirty && !$v.name.required}"
+        >
+        <label for="description">Имя</label>
+        <small
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+        >
+          Введите ваше имя
+        </small>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { required } from 'vuelidate/lib/validators'
+
+export default {
+  data: () => ({
+    name: ''
+  }),
+  metaInfo: {
+    title: 'Профиль | Bill.sh CRM'
+  },
+  validations: {
+    name: { required }
+  },
+  mounted () {
+    this.name = this.info.name
+    setTimeout(() => {
+      window.M.updateTextFields()
+    })
+  },
+  computed: {
+    ...mapGetters(['info'])
+  }
+}
+</script>
+
+<style scoped>
+.switch {
+  margin-bottom: 2rem;
+}
+</style>
